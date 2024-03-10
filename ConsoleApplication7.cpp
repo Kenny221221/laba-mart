0a5fce7649a413c5eba9292c63aa19ee6aebd5bb
Kenny221221 <150897228+Kenny221221@users.noreply.github.com>
2024-03-10 18:39:40 +0300

    Last commit

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <locale>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    string w;
+    cout << "Введите слово: ";
+    getline(cin, w);
+
+    unordered_map<char, int> lC;
+
+    for (char c : w) {
+        if (isalpha(c)) {
+            lC[c]++;
+        }
+    }
+
+    cout << "Количество различных букв: " << lC.size() << endl;
+
+    return 0;
+}
---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string str;
+    cout << "Введите строку: ";
+    getline(cin, str);
+
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+
+    string result = "";
+    for (int i = 0; i < str.length(); i++) {
+        if (result.find(str[i]) == string::npos) {
+            result += str[i];
+        }
+    }
+
+    cout << "Результат: " << result << endl;
+
+    return 0;
+}
